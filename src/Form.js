dbfd96c15be363a5df97fb9b78b4a27863874f08
Liliana Tuta <lilianatutza@gmail.com>
2024-12-16 18:18:00 +0000

    function, trial

--- src/Form.js
+++ src/Form.js
@@ -1,59 +1,61 @@
+// window.open("someLink", "_blank")
+
 import React from "react";
 
 export default function Form() {
- 
+  const [value, setValue] = useState(value);
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
-        <div className="Form">
-          <label for="books">Choose a book: </label>
-          <select id="books" >
-            <option value="">Select</option>
-            <option value="https://en.wikipedia.org/wiki/The_Colour_of_Magic">
-              The Colour of Magic
-            </option>
-            <option value="https://en.wikipedia.org/wiki/The_Light_Fantastic">
-              The Light Fantastic
-            </option>
-            <option value="">Equal Rites</option>
-            <option value="">Mort</option>
-            <option value="">Sorcery</option>
-            <option value="">Wyrd Sister</option>
-            <option value="">Pyramids</option>
-            <option value="">Gards!Guards!</option>
-            <option value="">Faust Eric</option>
-            <option value="">Moving Pictures</option>
-            <option value="">Reaper Man</option>
-            <option value="">Witches Abroad</option>
-            <option value="">Small Gods</option>
-            <option value="">Lords and Ladies</option>
-            <option value="">Men at Arms</option>
-            <option value="">Soul Music</option>
-            <option value="">Interesting Times</option>
-            <option value="">Maskerade</option>
-            <option value="">Feet of Clay</option>
-            <option value="">Hogfather</option>
-            <option value="">Jingo</option>
-            <option value="">The Last Continent</option>
-            <option value="">Carpe Jugulum</option>
-            <option value="">The Fifth Elephan</option>
-            <option value="">The Truth</option>
-            <option value="">Thief of Time</option>
-            <option value="">The Last Hero</option>
-            <option value="">The amazing Maurince And His Educated Rodents</option>
-            <option value="">Night Watch</option>
-            <option value="">The Free Wee Men</option>
-            <option value="">Monstrous Regiment</option>
-            <option value="">A Hat Full of Sky</option>
-            <option value="">Going Postal</option>
-            <option value="">Thud!</option>
-            <option value="">Wintersmith</option>
-            <option value="">Making Money</option>
-            <option value="">Unseen Academicals</option>
-            <option value="">I Shall Wear Midnight</option>
-            <option value="">Snuff</option>
-            <option value="">Raising Steam</option>
-            <option value="">The Shepherd's Crown</option>
-          </select>
-        </div>
-      );
-    }
-  
+    <div className="Form">
+      <label for="books">Choose a book: </label>
+      <select id="books">
+        <option value="">Select</option>
+        <option value="book">The Colour of Magic</option>
+        <option value="book">The Light Fantastic</option>
+        <option value="book">Equal Rites</option>
+        <option value="book">Mort</option>
+        <option value="book">Sorcery</option>
+        <option value="book">Wyrd Sister</option>
+        <option value="book">Pyramids</option>
+        <option value="book">Gards!Guards!</option>
+        <option value="book">Faust Eric</option>
+        <option value="book">Moving Pictures</option>
+        <option value="book">Reaper Man</option>
+        <option value="book">Witches Abroad</option>
+        <option value="book">Small Gods</option>
+        <option value="book">Lords and Ladies</option>
+        <option value="book">Men at Arms</option>
+        <option value="book">Soul Music</option>
+        <option value="book">Interesting Times</option>
+        <option value="book">Maskerade</option>
+        <option value="book">Feet of Clay</option>
+        <option value="book">Hogfather</option>
+        <option value="book">Jingo</option>
+        <option value="book">The Last Continent</option>
+        <option value="book">Carpe Jugulum</option>
+        <option value="book">The Fifth Elephan</option>
+        <option value="book">The Truth</option>
+        <option value="book">Thief of Time</option>
+        <option value="book">The Last Hero</option>
+        <option value="book">
+          The amazing Maurince And His Educated Rodents
+        </option>
+        <option value="book">Night Watch</option>
+        <option value="book">The Free Wee Men</option>
+        <option value="book">Monstrous Regiment</option>
+        <option value="book">A Hat Full of Sky</option>
+        <option value="book">Going Postal</option>
+        <option value="book">Thud!</option>
+        <option value="book">Wintersmith</option>
+        <option value="book">Making Money</option>
+        <option value="book">Unseen Academicals</option>
+        <option value="book">I Shall Wear Midnight</option>
+        <option value="book">Snuff</option>
+        <option value="book">Raising Steam</option>
+        <option value="book">The Shepherd's Crown</option>
+      </select>
+    </div>
+  );
+}
