5297ca5820c68c403664f9b7f86f35146a8e880e
Liliana Tuta <lilianatutza@gmail.com>
2024-12-16 13:21:34 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+        Hello from my project
+        
+        </p>
+        
+    </div>
+  );
+}
+
+export default App;
