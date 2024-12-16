52a7bb4d477ae3d619a3ae22489f743959810077
Liliana Tuta <lilianatutza@gmail.com>
2024-12-16 14:38:15 +0000

    applied css to the Form element

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import "./App.css";
+import Form from "./Form";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <p>Hello from my project</p>
-    
-    
-        <footer>
-        
-        This project was codded by <a
-          href="https://github.com/dashboard"
-          target="_blank"rel='noreferrer'>
-          Liliana Tuta</a>,{" "}is open sourced on <a
+      <Form />
+      <footer>
+        This project was codded by{" "}
+        <a href="https://github.com/dashboard" target="_blank" rel="noreferrer">
+          Liliana Tuta
+        </a>
+        , is open sourced on{" "}
+        <a
           href="https://github.com/lilianatutza/my-own-project"
-          target="_blank"rel='noreferrer'>  GitHub
-         </a> and is hosted on
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub
+        </a>{" "}
+        and is hosted on
         <a
           href="https://animated-clafoutis-a989e7.netlify.app/"
-          target="_blank"rel='noreferrer'> Netifly.
-          </a>
-          
-        </footer>
-        </div>
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netifly.
+        </a>
+      </footer>
+    </div>
   );
 }
-
-export default App;
