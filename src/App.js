a02fc638f2e83a1af31eca5645239ace6154faa4
Liliana Tuta <lilianatutza@gmail.com>
2024-12-16 13:39:33 +0000

    footer with links to git and hosting included

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-        <p>
-        Hello from my project
+      <p>Hello from my project</p>
+    
+    
+        <footer>
         
-        </p>
-        
-    </div>
+        This project was codded by <a
+          href="https://github.com/dashboard"
+          target="_blank"rel='noreferrer'>
+          Liliana Tuta</a>,{" "}is open sourced on <a
+          href="https://github.com/lilianatutza/my-own-project"
+          target="_blank"rel='noreferrer'>  GitHub
+         </a> and is hosted on
+        <a
+          href="https://animated-clafoutis-a989e7.netlify.app/"
+          target="_blank"rel='noreferrer'> Netifly.
+          </a>
+          
+        </footer>
+        </div>
   );
 }
 
